28e9d5b04e083c9ba7bde083361423efae3b5a96
Jarek Zawila <jaroslaw.zawila@gmail.com>
2023-05-01 15:18:27 +0100

    Improve implementation

--- src/main/scala/JudoPay.scala
+++ src/main/scala/JudoPay.scala
@@ -1,5 +1,32 @@
+import scala.collection.mutable
+
 object JudoPay {
   def lengthOfLongestSubstring(s: String) = {
-    s.toSet.size
+    s.foldLeft[(List[Char], Int)](List.empty, 0) {
+        case ((previous, maxLength), c) if !previous.contains(c) =>
+          (previous :+ c, Math.max(maxLength, previous.length + 1))
+
+        case ((previous, maxLength), c) =>
+          val removedDuplication = previous.dropWhile(_ != c).drop(1)
+          (
+            removedDuplication :+ c,
+            Math.max(maxLength, removedDuplication.length)
+          )
+      }
+      ._2
+  }
+
+  def lengthOfLongestSubstring2(s: String) = {
+    val characterIndex: mutable.Map[Char, Int] = mutable.HashMap[Char, Int]()
+
+    s.foldLeft((0, 0, 0)) {
+        case ((index1, index2, longest), next) =>
+          val ii = if (characterIndex.contains(next)) {
+            Math.max(characterIndex.getOrElse(next, -1), index2)
+          } else index2
+          characterIndex += (next -> (index1 + 1))
+          (index1 + 1, ii, Math.max(longest, index1 - ii + 1))
+      }
+      ._3
   }
 }
--- src/test/scala/JudoPayTest.scala
+++ src/test/scala/JudoPayTest.scala
@@ -1,32 +1,77 @@
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
+import java.time.Instant
 import scala.util.Random;
 
 class JudoPayTest extends AnyFlatSpec with Matchers {
 
   import JudoPay._;
 
-  "The longest string" should "return zero for empty string" in {
-    lengthOfLongestSubstring("") shouldBe 0
+  def lenghtOfLongerSubstringTest(name: String,
+                                  calculateLength: String => Int) {
+    it should "return zero for empty string" in {
+      calculateLength("") shouldBe 0
+    }
+
+    it should "return 1 for single element" in {
+      calculateLength("a") shouldBe 1
+    }
+
+    it should "return 1 for string of the same character" in {
+      val string =
+        List.iterate('A', Random.nextInt(1000))(identity).mkString("")
+
+      calculateLength(string) shouldBe 1
+    }
+
+    it should "handle 3 different characters" in {
+      calculateLength("abc") shouldBe 3
+    }
+
+    it should "handle repeated pattern of characters" in {
+      calculateLength("abcabcabcc") shouldBe 3
+    }
+
+    it should "handle 2 different characters" in {
+      calculateLength("au") shouldBe 2
+    }
+
+    it should "handle 2 different characters with multiplayed first" in {
+      calculateLength("aau") shouldBe 2
+    }
+
+    it should "handle repeated characters of different patterns" in {
+      calculateLength("ababbcbccdcddeded") shouldBe 2
+    }
+
+    it should "handle pwwkew string" in {
+      calculateLength("pwwkew") shouldBe 3
+    }
+
+    it should "handle aabaab!bb" in {
+      calculateLength("aabaab!bb") shouldBe 3
+    }
+
+    s"Execution time ${name}" should "printed" in {
+      val string = Random.alphanumeric.take(50000).mkString("")
+
+      val before = Instant.now().toEpochMilli;
+      calculateLength(string)
+      val time = Instant.now().toEpochMilli - before
+      println(time)
+
+    }
   }
 
-  it should "return 1 for single element" in {
-    lengthOfLongestSubstring("a") shouldBe 1
-  }
+  "Immutable implementation" should behave like lenghtOfLongerSubstringTest(
+    "immutable",
+    lengthOfLongestSubstring
+  )
 
-  it should "return 1 for string of the same character" in {
-    val string = List.iterate('A', Random.nextInt(1000))(identity).mkString("")
-
-    lengthOfLongestSubstring(string) shouldBe 1
-  }
-
-  it should "handle 3 different characters" in {
-    lengthOfLongestSubstring("abc") shouldBe 3
-  }
-
-  it should "handle repeated pattern of characters" in {
-    lengthOfLongestSubstring("abcabcabcc") shouldBe 3
-  }
+  "Mutable implementation" should behave like lenghtOfLongerSubstringTest(
+    "mutable",
+    lengthOfLongestSubstring2
+  )
 
 }
